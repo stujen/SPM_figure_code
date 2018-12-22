6c63983b76158f2717c4a378856788c59f98032b
Stuart <stuart.a.jenkins@gmail.com>
2018-12-22 10:59:58 +0000

    Update test_fair_scm.py

--- tests/test_fair_scm.py
+++ tests/test_fair_scm.py
@@ -126,7 +126,7 @@ def test_fair_scm_pulse():
 
 
 
-	emissions_input = np.zeros((1,100))
+	emissions_input = np.zeros((3,100))
 	emissions_input[0,30] = 30.
 
 	expected_C, expected_RF, expected_T = oxfair(emissions_input)
